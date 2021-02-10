f953946170925e5ab2752db7aaa639f0f2265881
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-02-10 09:56:08 +0530

    Added Blogs Page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import AboutPage from './pages/AboutPage';
 import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/contact-page/ContactPage';
+import BlogsPage from './pages/BlogsPage';
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 					<Route exact component={AboutPage} path="/about" />
 					<Route exact component={ProjectsPage} path="/projects" />
 					<Route exact component={ContactPage} path="/contact" />
+					<Route exact component={BlogsPage} path="/blogs" />
 				</Switch>
 			</Router>
 		</React.Fragment>
---
+++ src/pages/BlogsPage.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PageWrapper from '../layout/pagewrapper/PageWrapper';
+import styles from '../components/sections/blogs/blogs.module.css';
+import BlogCard from '../components/blog-card/BlogCard';
+
+const BlogsPage: React.FC<{}> = ({}) => {
+	return (
+		<div>
+			<PageWrapper>
+				<div className="section__title" style={{ marginTop: '6rem' }}>
+					<h1>latest articles</h1>
+				</div>
+				<div className={`section__center ${styles.blogs__section}`}>
+					<BlogCard
+						createdAt="May 25th, 2021"
+						image="blog-1"
+						title="Javascript Tricky Parts"
+						tags={[
+							'javascript'
+						]}
+					>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, ipsam nulla a cum quia quam nam
+						aut quod, blanditiis rerum sint reprehenderit sequi totam quasi?
+					</BlogCard>
+					<BlogCard
+						createdAt="March 18th, 2021"
+						image="blog-2"
+						title="NextJS - next big thing"
+						tags={[
+							'react',
+							'server-side-rendering',
+							'static sites',
+							'node JS'
+						]}
+					>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, ipsam nulla a cum quia quam nam
+						aut quod, blanditiis rerum sint reprehenderit sequi totam quasi?
+					</BlogCard>
+					<BlogCard
+						createdAt="April 6th, 2021"
+						image="blog-3"
+						title="SVG animations"
+						tags={[
+							'SVG',
+							'CSS',
+							'animations'
+						]}
+					>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, ipsam nulla a cum quia quam nam
+						aut quod, blanditiis rerum sint reprehenderit sequi totam quasi?
+					</BlogCard>
+					<BlogCard
+						createdAt="February 10th, 2021"
+						image="blog-4"
+						title="layouting with flutter"
+						tags={[
+							'flutter',
+							'mobile app development',
+							'layouts'
+						]}
+					>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, ipsam nulla a cum quia quam nam
+						aut quod, blanditiis rerum sint reprehenderit sequi totam quasi?
+					</BlogCard>
+				</div>
+			</PageWrapper>
+		</div>
+	);
+};
+
+export default BlogsPage;
