55caf8296ce07cca754f18726090984d2c2dd081
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-02-10 12:30:56 +0530

    Added page transition with framer motion.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import './css/normalize.css';
 import './css/App.css';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutPage from './pages/AboutPage';
 import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/contact-page/ContactPage';
 import BlogsPage from './pages/BlogsPage';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+	const location = useLocation();
 	return (
 		<React.Fragment>
-			<Router>
-				<Switch>
+			<AnimatePresence exitBeforeEnter>
+				<Switch location={location} key={location.pathname}>
 					<Route exact component={Home} path="/" />
 					<Route exact component={AboutPage} path="/about" />
 					<Route exact component={ProjectsPage} path="/projects" />
 					<Route exact component={ContactPage} path="/contact" />
 					<Route exact component={BlogsPage} path="/blogs" />
 				</Switch>
-			</Router>
+			</AnimatePresence>
 		</React.Fragment>
 	);
 }
--- src/layout/pagewrapper/PageWrapper.tsx
+++ src/layout/pagewrapper/PageWrapper.tsx
@@ -3,21 +3,34 @@ import Footer from '../footer/Footer';
 import Header from '../header/Header';
 import styles from './pagewrapper.module.css';
 import { Helmet } from 'react-helmet';
-
+import { motion } from 'framer-motion';
 interface PageWrapperProps {
 	title: string;
 }
 
+const pageVariant = {
+	visible:
+		{
+			width: '100vw',
+			opacity: 1
+		},
+	hidden:
+		{
+			width: 0,
+			opacity: 0
+		}
+};
+
 const PageWrapper: React.FC<PageWrapperProps> = ({ children, title }) => {
 	return (
-		<React.Fragment>
+		<motion.div variants={pageVariant} initial="hidden" animate="visible" exit={{ opacity: 0 }}>
 			<Helmet>
 				<title>{title}</title>
 			</Helmet>
 			<Header />
 			<div className={styles.content}>{children}</div>
 			<Footer />
-		</React.Fragment>
+		</motion.div>
 	);
 };
 
