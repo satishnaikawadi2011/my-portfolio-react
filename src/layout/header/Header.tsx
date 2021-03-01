c0f237306bdf082536782e83a9c0841044ace5c0
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-03-01 14:37:08 +0530

    Added new logo and favicon.

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import styles from './header.module.css';
 import { Link } from 'react-router-dom';
 import Menu from './Menu/Menu';
+import logo from './logo-vector.svg';
 
 interface HeaderProps {}
 
@@ -10,7 +11,8 @@ const Header: React.FC<HeaderProps> = () => {
 		<React.Fragment>
 			<nav className={styles.navbar} id="navbar">
 				<span className={styles.nav__logo}>
-					<Link to="/">
+					<Link to="/" className={styles.logo__header}>
+						<img src={logo} alt="logo" className={styles.logo} />
 						<h2>Satish</h2>
 					</Link>
 				</span>
--- src/layout/header/Menu/InfoSection/InfoSection.tsx
+++ src/layout/header/Menu/InfoSection/InfoSection.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Socialicons from '../../../../components/social-icons/Socialicons';
 import styles from './InfoSection.module.css';
+import logo from '../../logo-vector.svg';
 
 const InfoSection = () => {
 	return (
 		<div className={styles.hero__section}>
 			<div className={styles.logo}>
-				<h2>Logo</h2>
+				<img src={logo} alt="logo" />
 			</div>
 			<div className={styles.name}>
 				<h1>Satish Naikawadi</h1>
